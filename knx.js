4732aa1cdead796fa253e291f454f091033799a5
aborovsky <kycok.6o6pa@gmail.com>
2015-08-29 14:38:24 +0300

    first commit

---
+++ knx.js
@@ -0,0 +1,273 @@
+/**
+ * Created by aborovsky on 27.08.2015.
+ */
+
+var KnxConnectionTunneling = require('knx.js').KnxConnectionTunneling;
+
+function timestamp() {
+    return new Date().
+        toISOString().
+        replace(/T/, ' ').      // replace T with a space
+        replace(/\..+/, '')
+}
+function log(msg, args) {
+    if (args)
+        console.log(timestamp() + ': ' + msg, args);
+    else
+        console.log(timestamp() + ': ' + msg);
+}
+
+module.exports = function (RED) {
+
+    log("loading knx.js for node-red");
+    var knxjs = require('knx.js');
+
+    /**
+     * ====== Knx-CONTROLLER ================
+     * Holds configuration for knxjs host+port,
+     * initializes new knxjs connections
+     * =======================================
+     */
+    function KnxControllerNode(config) {
+        log("new KnxControllerNode, config: %j", config);
+        RED.nodes.createNode(this, config);
+        this.host = config.host;
+        this.port = config.port;
+        this.mode = config.mode;
+        this.knxjsconn = null;
+        var node = this;
+
+        /**
+         * Initialize an knxjs socket, calling the handler function
+         * when successfully connected, passing it the knxjs connection
+         */
+        this.initializeKnxConnection = function (handler) {
+            if (node.knxjsconn) {
+                log('already connected to knxjs server at ' + config.host + ':' + config.port + ' in mode[' + config.mode + ']');
+                if (handler && (typeof handler === 'function'))
+                    handler(node.knxjsconn);
+                return node.knxjsconn;
+            }
+            log('connecting to knxjs server at ' + config.host + ':' + config.port + ' in mode[' + config.mode + ']');
+            node.knxjsconn = null;
+            if (config.mode === 'tunnel/unicast') {
+                node.knxjsconn = new KnxConnectionTunneling(config.host, config.port, '0.0.0.0', 0);
+                node.knxjsconn.Connect(function (err) {
+                        if (handler && (typeof handler === 'function'))
+                            handler(node.knxjsconn);
+                        if (err) {
+                            log('connecting to knxjs server at ' + config.host + ':' + config.port + ' in mode[' + config.mode + ']');
+                            return null;
+                        }
+                        log('Knx: successfully connected to ' + config.host + ':' + config.port + ' in mode[' + config.mode + ']');
+                    }
+                );
+            }
+            else
+                throw 'Unsupported mode[' + config.mode + ']'
+            return node.knxjsconn;
+        };
+        this.on("close", function () {
+            log('disconnecting from knxjs server at %s:%d in mode[%s]', [config.host, config.port, config.mode]);
+            node.knxjsconn && node.knxjsconn.Disconnect && node.knxjsconn.Disconnect();
+        });
+    }
+
+    RED.nodes.registerType("knx-controller", KnxControllerNode);
+
+    /**
+     * ====== Knx-OUT =======================
+     * Sends outgoing KNX telegrams from
+     * messages received via node-red flows
+     * =======================================
+     */
+    function KnxOut(config) {
+        log('new Knx-OUT, config: %j', config);
+        RED.nodes.createNode(this, config);
+        this.name = config.name;
+        this.ctrl = RED.nodes.getNode(config.controller);
+        var node = this;
+        //
+        this.on("input", function (msg) {
+            log('knxout.onInput, msg=%j', msg);
+            if (!(msg && msg.hasOwnProperty('payload'))) return;
+            var payload;
+            if (typeof(msg.payload) === "object") {
+                payload = msg.payload;
+            } else if (typeof(msg.payload) === "string") {
+                payload = JSON.parse(msg.payload);
+            }
+            if (payload == null) {
+                log('knxout.onInput: illegal msg.payload!');
+                return;
+            }
+            var action;
+            switch (true) {
+                case /read/.test(msg.topic):
+                    action = 'read';
+                    break;
+                case /respon/.test(msg.topic):
+                    action = 'response';
+                    break;
+                default:
+                    action = 'write';
+            }
+            this.groupAddrSend(payload.dstgad, payload.value, payload.dpt, action, function (err) {
+                if (err) {
+                    log('groupAddrSend error: %j', err);
+                }
+            });
+
+        });
+        this.on("close", function () {
+            log('knxOut.close');
+        });
+
+        /**
+         * send a group write telegram to a group address
+         * Initializes new knxjs connection per request
+         * dstgad: dest group address '1/2/34'
+         * dpt: DataPointType eg. '1' for boolean
+         * value: the value to write
+         * callback:
+         *
+         * Usage:
+         * groupAddrSend({ host: 'localhost', port: 6720}, '1/2/34', 1, 1, function(err) {
+		*   if(err) console.error(err);
+		* });
+         *
+         * Datatypes:
+         *
+         KNX/EIB Function                   Information length      EIS         DPT     Value
+         Switch                             1 Bit                   EIS 1       DPT 1    0,1
+         Dimming (Position, Control, Value) 1 Bit, 4 Bit, 8 Bit     EIS 2        DPT 3    [0,0]...[1,7]
+         Time                               3 Byte                  EIS 3        DPT 10
+         Date                               3 Byte                  EIS 4       DPT 11
+         Floating point                     2 Byte                  EIS 5        DPT 9    -671088,64 - 670760,96
+         8-bit unsigned value               1 Byte                  EIS 6        DPT 5    0...255
+         8-bit unsigned value               1 Byte                  DPT 5.001    DPT 5.001    0...100
+         Blinds / Roller shutter            1 Bit                   EIS 7        DPT 1    0,1
+         Priority                           2 Bit                   EIS 8        DPT 2    [0,0]...[1,1]
+         IEEE Floating point                4 Byte                  EIS 9        DPT 14    4-Octet Float Value IEEE 754
+         16-bit unsigned value              2 Byte                  EIS 10        DPT 7    0...65535
+         16-bit signed value                2 Byte                  DPT 8        DPT 8    -32768...32767
+         32-bit unsigned value              4 Byte                  EIS 11        DPT 12    0...4294967295
+         32-bit signed value                4 Byte                  DPT 13        DPT 13    -2147483648...2147483647
+         Access control                     1 Byte                  EIS 12        DPT 15
+         ASCII character                    1 Byte                  EIS 13        DPT 4
+         8859_1 character                   1 Byte                  DPT 4.002    DPT 4.002
+         8-bit signed value                 1 Byte                  EIS 14        DPT 6    -128...127
+         14 character ASCII                 14 Byte                 EIS 15        DPT 16
+         14 character 8859_1                14 Byte                 DPT 16.001    DPT 16.001
+         Scene                              1 Byte                  DPT 17        DPT 17    0...63
+         HVAC                               1 Byte                  DPT 20        DPT 20    0..255
+         Unlimited string 8859_1            .                       DPT 24        DPT 24
+         List 3-byte value                  3 Byte                  DPT 232        DPT 232    RGB[0,0,0]...[255,255,255]
+         *
+         */
+        this.groupAddrSend = function (dstgad, value, dpt, action, callback) {
+            dpt = dpt.toString();
+            if (action !== 'write')
+                throw 'Unsupported action[' + action + '] inside of groupAddrSend';
+            log('groupAddrSend action[' + action + '] dstgad:' + dstgad + ', value:' + value + ', dpt:' + dpt);
+            switch (dpt) {
+                case '1': //Switch
+                    value = (value.toString() === 'true' || value.toString() === '1')
+                    break;
+                case '9': //Floating point
+                    value = parseFloat(value);
+                    break;
+                case '5':    //8-bit unsigned value               1 Byte                  EIS 6         DPT 5    0...255
+                case '5.001':    //8-bit unsigned value               1 Byte                  DPT 5.001    DPT 5.001    0...100
+                case '6':    //8-bit signed value                 1 Byte                  EIS 14        DPT 6    -128...127
+                case '7':    //16-bit unsigned value              2 Byte                  EIS 10        DPT 7    0...65535
+                case '8':    //16-bit signed value                2 Byte                  DPT 8         DPT 8    -32768...32767
+                case '12':   //32-bit unsigned value              4 Byte                  EIS 11        DPT 12    0...4294967295
+                case '13':   //32-bit signed value                4 Byte                  DPT 13        DPT 13    -2147483648...2147483647
+                case '17':   //Scene                              1 Byte                  DPT 17        DPT 17    0...63
+                case '20':   //HVAC                               1 Byte                  DPT 20        DPT 20    0..255
+                    value = parseInt(value);
+                    break;
+                default:
+                    throw 'Unsupported dpt[' + dpt + '] inside groupAddrSend of knx node'
+
+            }
+
+            // init a new one-off connection from the effectively singleton KnxController
+            // there seems to be no way to reuse the outgoing conn in adreek/node-knxjs
+            this.ctrl.initializeKnxConnection(function (connection) {
+                try {
+                    log("sendAPDU: %j", JSON.stringify(value));
+                    connection.Action(dstgad.toString(), value, dpt);
+                    callback && callback();
+                }
+                catch (err) {
+                    log('error calling groupAddrSend!: %j', err);
+                    callback(err);
+                }
+            });
+        }
+    }
+
+    //
+    RED.nodes.registerType("knx-out", KnxOut);
+
+    /**
+     * ====== KNX-IN ========================
+     * Handles incoming KNX events, injecting
+     * json into node-red flows
+     * =======================================
+     */
+    function KnxIn(config) {
+        log('new KNX-IN, config: %j', config);
+        RED.nodes.createNode(this, config);
+        this.name = config.name;
+        this.inconn = null;
+        var node = this;
+        var knxjsController = RED.nodes.getNode(config.controller);
+        /* ===== Node-Red events ===== */
+        this.on("input", function (msg) {
+            if (msg != null) {
+
+            }
+            ;
+        });
+        this.on("close", function () {
+            log('knxjsIn.close');
+            if (this.inconn) {
+                log('closing knxjsIn connection');
+                this.inconn.end();
+            }
+        });
+//		this.on("error", function(msg) {});
+
+        /* ===== knxjs events ===== */
+        // initialize incoming KNX event socket (openGroupSocket)
+        // there's only one connection for knxjs-in:
+        knxjsController.initializeKnxConnection(function (conn) {
+            this.inconn = conn;
+            this.inconn.openGroupSocket(0, function (parser) {
+                parser.on('write', function (src, dest, dpt, val) {
+                    log('Write from ' + src + ' to ' + dest + ': ' + val);
+                    node.send({
+                        topic: 'knx: write',
+                        payload: {'srcphy': src, 'dstgad': dest, 'dpt': dpt, 'value': val}
+                    });
+                });
+                //
+                parser.on('response', function (src, dest, val) {
+                    log('Response from %s to %s: %s', src, dest, val);
+                    node.send({topic: 'knx: response', payload: {'srcphy': src, 'dstgad': dest, 'value': val}});
+                });
+                //
+                parser.on('read', function (src, dest) {
+                    log('Read from %s to %s', src, dest);
+                    node.send({topic: 'knx: read', payload: {'srcphy': src, 'dstgad': dest}});
+                });
+            });
+        });
+    }
+
+    //
+    RED.nodes.registerType("knx-in", KnxIn);
+}
